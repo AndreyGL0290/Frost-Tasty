260e899ca9316a854a46f39a55f58dc967778db6
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-01-10 21:09:38 +0400

    Copied working code

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-let tg = window.Telegram.WebApp
+let tg = window.Telegram.WebApp;
 
 tg.expand();
 
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(200);
+	tg.sendData();
 });
+
+let usercard = document.getElementsByClassName("usercard")[0];
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+
+usercard.appendChild(p); 
