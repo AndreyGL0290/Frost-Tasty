bf2aaaf2763edac671caabfcfbc7f906207738f3
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-01-29 13:14:06 +0400

    Replaced into js folder

--- telegram.js
+++
@@ -1,8 +0,0 @@
-import {basket} from './build_menu.js'
-export let tg = window.Telegram.WebApp
-
-tg.expand();
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(JSON.stringify(basket.products));
-});
