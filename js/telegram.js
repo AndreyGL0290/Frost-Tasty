608bf54311312d0eccdc0faea556d9f199330c14
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-04 20:59:41 +0400

    Added telegram back button

--- js/telegram.js
+++ js/telegram.js
@@ -4,4 +4,7 @@ export let tg = window.Telegram.WebApp
 
 tg.expand();
 
+tg.BackButton.show()
+// tg.BackButton.onClick(() => {console.log('back button clicked')})
+
 tg.onEvent("mainButtonClicked", () => {tg.sendData(JSON.stringify(basket.products))});
