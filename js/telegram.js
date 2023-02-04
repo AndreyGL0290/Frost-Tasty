117d69f874d205c599c6c654beeeea6f9f09789d
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-04 21:22:07 +0400

    Wrote telegram back button logic

--- js/telegram.js
+++ js/telegram.js
@@ -1,11 +1,17 @@
 import {basket} from './basket.js'
+import {routes} from './urlRoute.js';
 
 export let tg = window.Telegram.WebApp
 
 tg.expand();
+
 tg.MainButton.color = '#FFBF00'
 tg.MainButton.textColor = '#000000'
-tg.BackButton.show()
-// tg.BackButton.onClick(() => {console.log('back button clicked')})
+tg.MainButton.onClick(() => {
+    tg.sendData(JSON.stringify(basket.products))
+});
 
-tg.onEvent("mainButtonClicked", () => {tg.sendData(JSON.stringify(basket.products))});
+tg.BackButton.onClick(() => {
+    document.getElementById('root').innerHTML = ''
+    routes['/Frost-Tasty_html_pages/'].constructor.func(routes['/Frost-Tasty_html_pages/'].constructor.props)
+})
