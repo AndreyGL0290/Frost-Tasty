927989501e0c981e16551235ba6857535ac59159
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-04 21:06:13 +0400

    Changed main button design

--- js/telegram.js
+++ js/telegram.js
@@ -3,7 +3,8 @@ import {basket} from './basket.js'
 export let tg = window.Telegram.WebApp
 
 tg.expand();
-
+tg.MainButton.color = '#FFBF00'
+tg.MainButton.textColor = '#000000'
 tg.BackButton.show()
 // tg.BackButton.onClick(() => {console.log('back button clicked')})
 
