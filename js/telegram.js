a17d76aedff04bc980d81781b086cdadf68dab00
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-05 22:02:07 +0400

    Changed main button onclick function structure

--- js/telegram.js
+++ js/telegram.js
@@ -7,8 +7,9 @@ tg.expand();
 
 tg.MainButton.color = '#FFBF00'
 tg.MainButton.textColor = '#000000'
-tg.MainButton.onClick(() => {
+Telegram.WevApp.onEvent('mainButtonClicked', () => {
     let data = {}
+
     // Shortens basket product data so only valuable information is send
     for (let i = 0; i < Object.keys(basket.products).length; i += 1){
         data[Object.keys(basket.products)[i]] = {'price': basket.products[Object.keys(basket.products)[i]].price, 'quantity': basket.products[Object.keys(basket.products)[i]].quantity.get()}
