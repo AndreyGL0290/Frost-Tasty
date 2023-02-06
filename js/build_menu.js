cefe14d9a818af1dddbf2a79164239a2d6b975d7
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-06 22:19:26 +0400

    Simplified development

--- js/build_menu.js
+++ js/build_menu.js
@@ -30,7 +30,7 @@ export function createGroupCards(menu){
 
         let button = document.createElement('a')
 
-        button.href = urlPath + '/products#' + Object.keys(menu)[i]
+        button.href = window.location.pathname + 'products#' + Object.keys(menu)[i]
         button.className = 'card-button groups'
         button.textContent = 'Перейти'
 
@@ -123,7 +123,7 @@ export function createBasketButton(){
     let basketButton = document.createElement('a')
     basketButton.className = 'basket-button'
     basketButton.textContent = "Continue"
-    basketButton.href = "/basket"
+    basketButton.href = window.location.pathname + "basket"
 
     basketButtonContainer.appendChild(basketButton)
 
@@ -202,7 +202,7 @@ export function createBasketMenu(products){
                 let getMoreButton = document.createElement('a')
                 getMoreButton.className = 'get-more-button'
                 getMoreButton.innerHTML = 'Посмотреть<br><br>'
-                getMoreButton.href = urlPath + '/'
+                getMoreButton.href = window.location.pathname
                 
                 afterLabelContainer.appendChild(afterLabel)
                 afterLabelContainer.appendChild(getMoreButton)
@@ -231,7 +231,7 @@ export function createBasketMenu(products){
         // Eye button
         let showProductButton = document.createElement('a')
         showProductButton.className = 'show-product-button'
-        showProductButton.href = 'products#' + products[Object.keys(products)[i]].parent
+        showProductButton.href = window.location.pathname + 'products#' + products[Object.keys(products)[i]].parent
 
         // Product name label
         let productLabel = document.createElement('span')
