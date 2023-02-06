72fa020316099daa169e3c8886ffbb069482ed27
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2023-02-06 21:30:46 +0400

    Simplified development

--- js/build_menu.js
+++ js/build_menu.js
@@ -123,7 +123,7 @@ export function createBasketButton(){
     let basketButton = document.createElement('a')
     basketButton.className = 'basket-button'
     basketButton.textContent = "Continue"
-    basketButton.href = urlPath + "/basket"
+    basketButton.href = "/basket"
 
     basketButtonContainer.appendChild(basketButton)
 
